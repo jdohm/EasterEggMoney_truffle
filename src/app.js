993e5f5c1d3ce86fe80398aa08aa4a1736875e9d
Jannis Dohm <jannismax@gmail.com>
2021-04-19 04:49:59 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -13,27 +13,35 @@ App = {
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
-        App.web3Provider = web3.currentProvider;
-        web3 = new Web3(web3.currentProvider);
+        App.web3Provider = ethereum;
+        web3 = new Web3(ethereum);
     } else {
         window.alert("Please connect to Metamask.");
     }
     // Modern dapp browsers...
     if (window.ethereum) {
         window.web3 = new Web3(ethereum);
-      try {
-        // Request account access if needed
-          await ethereum.enable();
-        // Acccounts now exposed
-          web3.eth.sendTransaction({/* ... */});
-      } catch (error) {
-        // User denied account access...
-      }
+        try {
+            // Request account access if needed
+            const accounts = await ethereum.send('eth_requestAccounts');
+            // Accounts now exposed, use them
+            ethereum.send('eth_sendTransaction', { from: accounts[0], /* ... */ })
+        } catch (error) {
+            // User denied account access
+}
+      //try {
+        //// Request account access if needed
+          //await ethereum.enable();
+        //// Acccounts now exposed
+          //web3.eth.sendTransaction({/* ... */});
+      //} catch (error) {
+        //// User denied account access...
+      //}
     }
     // Legacy dapp browsers...
     else if (window.web3) {
-        App.web3Provider = web3.currentProvider;
-      window.web3 = new Web3(web3.currentProvider);
+        App.web3Provider = ethereum;
+      window.web3 = new Web3(ethereum);
       // Acccounts always exposed
         web3.eth.sendTransaction({/* ... */});
     }
