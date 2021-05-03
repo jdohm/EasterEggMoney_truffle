73b09dd45abac7e85eaee0ba6928de3473ecb76a
Jannis Dohm <jannismax@gmail.com>
2021-05-04 01:53:02 +0200

    gsn tryouts (not working!)
    abandoned since, requiring the user to pay the fees prevents drainage resulting
    in people trying to solve the password without finding the Easter egg.
    
    New users can use a faucet to get started and receive the Easter egg if they find it.

--- contracts/EasterEggMoney.sol
+++ contracts/EasterEggMoney.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MPL-2.0
-pragma solidity 0.8.3;
+pragma solidity 0.6.2;
 
-contract EasterEggMoney{
+//import "@openzeppelin/contracts/GSN/GSNRecipient.sol";
+//import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
+import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
+
+
+contract EasterEggMoney is BaseRelayRecipient{
     bytes32 passHash;
     address owner;
     uint public findersCount = 0;
@@ -10,9 +15,12 @@ contract EasterEggMoney{
     //you can get the hash value from https://www.keccak-256.cloxy.net/ for example
     //choose a strong password!
     // test = 9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658
-    constructor(bytes32 _passHash) payable {
+    constructor(bytes32 _passHash) public payable {
         passHash = _passHash;
-        owner = msg.sender;
+        owner =  _msgSender;
+  /*       constructor(address _forwarder) public { */
+  /*           trustedForwarder = _forwarder; */
+	/* } */
     }
 
     //function to add new funds to EasterEggMoney pool for new finders to be rewarded
@@ -35,14 +43,41 @@ contract EasterEggMoney{
         if(keccak256(bytes(_passphrase)) == passHash){
         findersCount ++;
         //send available funds to adress
-        payable(msg.sender).transfer(address(this).balance);
+        payable(_msgSender).transfer(address(this).balance);
         }
         return (findersCount, address(this).balance);
     }
 
     //function to update passphrase after EasterEgg was found
     function updatepw(bytes32 _passHash) payable public {
-    require(msg.sender == owner);
+    require(_msgSender == owner);
     passHash = _passHash;
     }
+
+    function preRelayedCall(
+                            GSNTypes.RelayRequest relayRequest,
+                            bytes approvalData,
+                            uint256 maxPossibleGas
+                            )
+        external
+        returns (
+                 bytes memory context,
+                 bool rejectOnRecipientRevert
+                 );
+
+    function postRelayedCall(
+                             bytes context,
+                             bool success,
+                             bytes32 preRetVal,
+                             uint256 gasUseWithoutPost,
+                             GSNTypes.GasData calldata gasData
+                             ) external;
+
+    /* function getGasLimits() */
+    /*     external */
+    /*     view */
+    /*     returns ( */
+    /*              GasLimits memory limits */
+    /*              ); */
+
 }
